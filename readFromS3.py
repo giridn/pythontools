dd7c03737bd43f86a249192b61f2b2ff8cc15296
giridn <41450477+giridn@users.noreply.github.com>
2019-06-04 16:57:20 -0400

    #1 Read from s3

---
+++ readFromS3.py
@@ -0,0 +1,30 @@
+import os
+
+import boto3
+
+# References:
+#https://realpython.com/python-boto3-aws-s3/
+
+
+AWS_ACCESS_KEY_ID = os.environ['aws_access_key_id']
+AWS_SECRET_ACCESS_KEY = os.environ['aws_secret_access_key']
+
+
+# Read from S3
+BUCKET_NAME='datasample1'
+KEY='blood_data_cleaned.csv'
+print(AWS_ACCESS_KEY_ID, "~", AWS_SECRET_ACCESS_KEY)
+session = boto3.Session(
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+)
+s3_resource = session.resource('s3')
+my_bucket = s3_resource.Bucket(BUCKET_NAME) #subsitute this for your s3 bucket name.
+
+
+files = list(my_bucket.objects.filter(Prefix=KEY))
+file = files[0]
+print(file.key)
+body=file.get()['Body'].read()
+print(body)
+
