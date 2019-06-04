3449bfc9187c2b76354cf974fff2134c66706e36
naren <narendhirang@gmail.com>
2019-06-04 17:02:34 -0400

    #2 Remove credentials print statement

--- readFromS3.py
+++ readFromS3.py
@@ -1,10 +1,8 @@
 import os
-
 import boto3
 
 # References:
 #https://realpython.com/python-boto3-aws-s3/
-
 
 AWS_ACCESS_KEY_ID = os.environ['aws_access_key_id']
 AWS_SECRET_ACCESS_KEY = os.environ['aws_secret_access_key']
@@ -13,7 +11,7 @@ AWS_SECRET_ACCESS_KEY = os.environ['aws_secret_access_key']
 # Read from S3
 BUCKET_NAME='datasample1'
 KEY='blood_data_cleaned.csv'
-print(AWS_ACCESS_KEY_ID, "~", AWS_SECRET_ACCESS_KEY)
+
 session = boto3.Session(
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
